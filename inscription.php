dfa3dfe9dea65a04b8c90ab35ad0376baf2be570
VincentF <vincent.fagioli@cpnv.ch>
2018-03-09 14:54:41 +0100

    Connexion DataBase
    Ajout du code permettant de ce connecter à la DataBase local sur phpmyadmin

--- inscription.php
+++ inscription.php
@@ -1,10 +1,19 @@
 <?php
-//Description:
+//Description: Page d'inscription
 // Date:09.03.2018
 // Auteur:Vincent Fagioli
  
 //########################## Connexion DB ############################################
-
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=tpi;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
 //########################## Traitement (SESSION) ############################################
 
 //########################## Traitement (GET) ############################################
