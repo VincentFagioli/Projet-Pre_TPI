ca6f3c8c56b083f9fb2b77657a7df1c3b77131c8
VincentF <vincent.fagioli@cpnv.ch>
2018-03-09 14:44:32 +0100

    Canevas commentaire
    Ajout du canevas pour les commentaires et l'organisation du code

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+//Description:index du site
+// Date: 09.03.2018
+// Auteur:Vincent Fagioli
+ 
+//########################## Connexion DB ############################################
 
+//########################## Traitement (SESSION) ############################################
+
+//########################## Traitement (GET) ############################################
+
+//########################## Traitement (POST) ############################################
+
+//########################## Affichage du contenu de la page ##############################
 echo '<html> <h3>Bienvenue, pour vous inscrire ou vous connecter utliser les 2 liens ci-dessous</h3><a href="connexion.php">connexion</a>
 <a href="inscription.php">inscription</a></html>';
 ?>
