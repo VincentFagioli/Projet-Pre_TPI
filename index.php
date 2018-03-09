993040c1f063d18c8e106354cf4ac15031525478
haduken <vincent.fagioli@cpnv.ch>
2018-03-09 14:36:46 +0100

    Liens inscription/connexion
    Ajout d'un petit message d'accueil et des liens redirigeant sur inscription.php et connexion.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<html> <h3>Bienvenue, pour vous inscrire ou vous connecter utliser les 2 liens ci-dessous</h3><a href="connexion.php">connexion</a>
+<a href="inscription.php">inscription</a></html>';
+?>
+
