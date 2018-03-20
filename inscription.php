12914bb112cabb18ac3ecb9554b5fd2a83ed20db
VincentF <vincent.fagioli@cpnv.ch>
2018-03-20 08:52:33 +0100

    Vérification non redondance username
    Ajout de la vérification de disponibilité du pseudo à chaque inscription

--- inscription.php
+++ inscription.php
@@ -23,12 +23,24 @@ extract($_POST); // $username $password
 
 if(isset($username) && isset($password))
 {
-    $passwordHach= password_hash($password,PASSWORD_DEFAULT);
-    $stmt = $bdd->prepare("INSERT INTO `user` (`ID_user`, `title`, `firstname`, `lastname`, `username`, `password`, `email`, `phonenumber`, `adress`, `npa`, `locality`) VALUES (NULL, '', '', '', ?, ?, '', NULL, '', NULL, '')");  
-    $stmt->bindParam(1, $username);
-    $stmt->bindParam(2, $passwordHach);
-    $stmt->execute();
-    echo"<script language=\"javascript\"> alert('inscription réussie') </script>";
+        //On obtient une table existe qui contient le nombre d'entrée correspondant au couple pseudo password
+        $query = "SELECT COUNT(*) AS existe FROM user WHERE `username` LIKE '$username'";
+        $stmt = $bdd->query($query) or die ("<br>SQL Error in: <br> $query<br>Error message:".$bdd->errorInfo()[2]); 
+        $ident = $stmt->fetch();
+        // Si y a une entrée ou plus cela veut dire que le pseudo et déjà utliser, on va alors afficher un message d'erreur, sinon on inscrit les données dans la database
+        if($ident['existe']>0)
+        {
+            echo"<script language=\"javascript\"> alert('Pseudo déjà utilisé') </script>";
+        }
+        else
+        {
+                $passwordHach= password_hash($password,PASSWORD_DEFAULT);
+                $stmt = $bdd->prepare("INSERT INTO `user` (`ID_user`, `title`, `firstname`, `lastname`, `username`, `password`, `email`, `phonenumber`, `adress`, `npa`, `locality`) VALUES (NULL, '', '', '', ?, ?, '', NULL, '', NULL, '')");  
+                $stmt->bindParam(1, $username);
+                $stmt->bindParam(2, $passwordHach);
+                $stmt->execute();
+                echo"<script language=\"javascript\"> alert('inscription réussie') </script>";
+        }
 }
 //########################## Affichage du contenu de la page ##############################
 ?>
